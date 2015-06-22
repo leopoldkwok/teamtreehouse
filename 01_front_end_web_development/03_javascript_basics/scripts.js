05893024b076f0dd1f42d2b636803a3c33c8851b
leopoldkwok <leopoldkwok@gmail.com>
2015-06-22 20:50:29 +0100

    Working with Strings and finding help

--- 01_front_end_web_development/03_javascript_basics/scripts.js
+++ 01_front_end_web_development/03_javascript_basics/scripts.js
@@ -1,9 +1,21 @@
-var visitor = prompt("What is your name?");
-var message = 'Hello ' + visitor + ". Welcome to Treehouse.";
-message += "We are so glad that you came by to visit, ";
-message += visitor;
-message += ". Please come again, when you want to learn some more.";
-document.write(message);
+var stringToShout = prompt("What should I shout?");
+var shout = stringToShout.toUpperCase();
+shout +="!!!";
+alert(shout);
+
+
+// var passphrase = "Open Sesame";
+// console.log(passphrase.length);
+// console.log(passphrase.toLowerCase());
+// console.log(passphrase);
+// console.log(passphrase.toUpperCase());
+
+// var visitor = prompt("What is your name?");
+// var message = 'Hello ' + visitor + ". Welcome to Treehouse.";
+// message += "We are so glad that you came by to visit, ";
+// message += visitor;
+// message += ". Please come again, when you want to learn some more.";
+// document.write(message);
 
 
 // var visitorName = prompt('What is your name?');
