f17e626368ecc59e0cfa5b4a76a69ba59f3ab091
leopoldkwok <leopoldkwok@gmail.com>
2015-06-22 23:18:56 +0100

    Numbers and Strings

--- 01_front_end_web_development/03_javascript_basics/js_basics_stage_3/scripts.js
+++ 01_front_end_web_development/03_javascript_basics/js_basics_stage_3/scripts.js
@@ -1,12 +1,21 @@
-var secondsPerMin = 60;
-var minsPerHour = 60;
-var hoursPerDay = 24;
-var daysPerWeek = 7;
-var weeksPerYear = 52;
-var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
-document.write('There are ' + secondsPerDay + ' seconds in a day.');
 
-var yearsAlive = prompt("How old are you?");
-var yearsAliveCalc = yearsAlive * secondsPerDay * daysPerWeek * weeksPerYear;
+var HTMLBadges = prompt('How many HTML badges do you have?');
 
-document.write("<h2>You have been alive for " + yearsAliveCalc + " seconds</h2>");
+var CSSBadges = prompt('How many CSS badges do you have?');
+
+var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+alert("Wow! You have " + totalBadges + " badges!");
+
+
+// var secondsPerMin = 60;
+// var minsPerHour = 60;
+// var hoursPerDay = 24;
+// var daysPerWeek = 7;
+// var weeksPerYear = 52;
+// var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+// document.write('There are ' + secondsPerDay + ' seconds in a day.');
+
+// var yearsAlive = prompt("How old are you?");
+// var yearsAliveCalc = yearsAlive * secondsPerDay * daysPerWeek * weeksPerYear;
+
+// document.write("<h2>You have been alive for " + yearsAliveCalc + " seconds</h2>");
