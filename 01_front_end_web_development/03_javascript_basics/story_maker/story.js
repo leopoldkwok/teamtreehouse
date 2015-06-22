069def52cd007925681b3494149900874a008d1d
leopoldkwok <leopoldkwok@gmail.com>
2015-06-22 23:31:01 +0100

    The Mad Libs Challenge Revisited

--- 01_front_end_web_development/03_javascript_basics/story_maker/story.js
+++ 01_front_end_web_development/03_javascript_basics/story_maker/story.js
@@ -1,10 +1,16 @@
-"<h2>There once was a [adjective] programmer who wanted to use Javascript to [verb] the [noun].</h2>"
-
-var adjective = prompt('Please type an adjective');
-var sentence = "<h2>There once was a " + adjective;
-var verb = prompt('Please type a verb');
-var noun = prompt('Please type a noun');
+var questions = 3;
+var questionsLeft = '[' + questions + ' questions left]';
+var adjective = prompt('Please type an adjective ' + questionsLeft);
+questions -= 1;
+questionsLeft = '[' + questions + ' questions left]';
+var verb = prompt('Please type a verb' + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt('Please type a noun ' + questionsLeft);
 alert('All done. Ready for the message?');
+var sentence = "<h2>There once was a " + adjective;
+
+
 sentence += ' programmer who wanted to use Javascript to ' + verb;
 sentence += ' the ' + noun + '.</h2>';
 document.write(sentence);
