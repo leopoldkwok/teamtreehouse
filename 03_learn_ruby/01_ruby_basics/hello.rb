21dd3db31128141e522b34e31b4acf20e6741827
leopoldkwok <leopoldkwok@gmail.com>
2015-06-24 00:15:09 +0100

    practising with numbers

--- 03_learn_ruby/01_ruby_basics/hello.rb
+++ 03_learn_ruby/01_ruby_basics/hello.rb
@@ -1,4 +1,10 @@
-print "Please enter your name: "
-name = gets
+# print "Please enter your name: "
+name = "Jason"
+string = "My name is #{name}"
+puts string
 
-puts "Hello #{name}!"
+year = 2014
+puts "The year is #{year}"
+
+future = 5
+puts "In #{future} years, the year will be #{year + future}"
