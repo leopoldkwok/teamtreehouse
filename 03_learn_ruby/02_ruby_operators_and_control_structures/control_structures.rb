0e328fe3ef404399d52a92680265a2d1bf969deb
leopoldkwok <leopoldkwok@gmail.com>
2015-06-24 09:16:54 +0100

    Ruby if statement

---
+++ 03_learn_ruby/02_ruby_operators_and_control_structures/control_structures.rb
@@ -0,0 +1,11 @@
+car1_speed = 500
+car2_speed = 300
+car2_speed += 300
+
+if car1_speed < car2_speed
+	puts "Car 1 is slower than car 2."
+end
+
+if car2_speed > car1_speed
+	puts "Car 2 is faster than car 2."
+end
