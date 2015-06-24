db7103f5d1bbef4b4f1617d34b93dab8ab874be6
leopoldkwok <leopoldkwok@gmail.com>
2015-06-24 09:31:59 +0100

    The Ruby Else statement

--- 03_learn_ruby/02_ruby_operators_and_control_structures/control_structures.rb
+++ 03_learn_ruby/02_ruby_operators_and_control_structures/control_structures.rb
@@ -1,11 +1,30 @@
-car1_speed = 500
-car2_speed = 300
-car2_speed += 300
+print "Enter name: "
+name = gets.chomp
 
-if car1_speed < car2_speed
-	puts "Car 1 is slower than car 2."
+if name == "Jason"
+	puts "That's my name, too!"
+else 
+	puts "Hi #{name}!"
+end	
+
+print "Would you like to see your name backwards? (yes or no)"
+answer = gets.chomp.downcase
+
+if answer == "yes"
+	puts "This is your name backwards:"
+	puts name.reverse
+else
+	puts "Ok, maybe later."
 end
 
-if car2_speed > car1_speed
-	puts "Car 2 is faster than car 2."
-end
+# car1_speed = 500
+# car2_speed = 300
+# car2_speed += 300
+
+# if car1_speed < car2_speed
+# 	puts "Car 1 is slower than car 2."
+# end
+
+# if car2_speed > car1_speed
+# 	puts "Car 2 is faster than car 2."
+# end
