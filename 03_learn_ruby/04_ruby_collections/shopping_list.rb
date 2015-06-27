85ae1c5a2b6343446eed4133090e74297c68de80
leopoldkwok <leopoldkwok@gmail.com>
2015-06-27 12:38:11 +0100

    Build a Grocery List Program: Part 2

--- 03_learn_ruby/04_ruby_collections/shopping_list.rb
+++ 03_learn_ruby/04_ruby_collections/shopping_list.rb
@@ -7,10 +7,18 @@ def create_list
 end
 
 def add_list_item
-	print "What is the item called?"
-	item_name => name }
+	print "What is the item called? "
+	item_name = gets.chomp
+
+	print "How much? "
+	quantity = gets.chomp.to_i
+
+	hash = { "name" => item_name, "quantity" => quantity }
 	return hash
 end
 
 list = create_list()
+puts list.inspect
+list['items'].push(add_list_item())
+
 puts list.inspect
