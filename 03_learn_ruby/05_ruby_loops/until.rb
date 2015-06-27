8ebc211c0f9442b5655acbc0a9cefee52972d59b
leopoldkwok <leopoldkwok@gmail.com>
2015-06-27 20:57:42 +0100

    The Until Loop

---
+++ 03_learn_ruby/05_ruby_loops/until.rb
@@ -0,0 +1,5 @@
+answer = ""
+until answer == "no" do 
+	print "Do you want this loop to continue? (y/n) "
+	answer = gets.chomp
+end
---
+++ 03_learn_ruby/05_ruby_loops/until_number.rb
@@ -0,0 +1,15 @@
+def print_hello(number_of_times)
+	i = 0
+	while i < number_of_times
+		puts "Hello"
+		i += 1
+	end
+end
+
+answer = 0
+
+until answer >= 5
+	print "How many times do you want to print 'hello'? Enter a number greater than 5 to exit) "
+	answer = gets.chomp.to_i
+	print_hello(answer)
+end
