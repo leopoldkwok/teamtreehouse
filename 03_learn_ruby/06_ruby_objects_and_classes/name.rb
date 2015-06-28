4d50d59a2756e7eeaf2a9032c906afeafed8db58
leopoldkwok <leopoldkwok@gmail.com>
2015-06-28 19:08:30 +0100

    Attribute Writers and Accessors

--- 03_learn_ruby/06_ruby_objects_and_classes/name.rb
+++ 03_learn_ruby/06_ruby_objects_and_classes/name.rb
@@ -1,5 +1,6 @@
 class Name
-	attr_reader :title, :first_name, :middle_name, :last_name
+	attr_reader :first_name, :middle_name, :last_name
+	attr_accessor:title
 
 
 	def initialize(title, first_name, middle_name, last_name)
@@ -10,8 +11,14 @@ class Name
 	end
 end
 
+
+
 name = Name.new("Mr.", "Jason", "", "Seifer")
 puts name.title + " " +
 	 name.first_name + " " +
 	 name.middle_name + " " +
 	 name.last_name
+
+puts "Title: #{name.title}"
+name.title = "Dr."
+puts "Title: #{name.title}"
