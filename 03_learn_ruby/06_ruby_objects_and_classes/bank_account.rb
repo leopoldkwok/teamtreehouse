d613527ad7b227fc5dcd02d9507639c365d00f70
leopoldkwok <leopoldkwok@gmail.com>
2015-06-28 20:41:07 +0100

    Part 3: Keeping Our Balance

--- 03_learn_ruby/06_ruby_objects_and_classes/bank_account.rb
+++ 03_learn_ruby/06_ruby_objects_and_classes/bank_account.rb
@@ -18,12 +18,25 @@ class BankAccount
 	def add_transaction(description, amount)
 		@transactions.push(description: description, amount: amount)
 	end
+
+	def balance
+		balance = 0.0
+		@transactions.each do |transaction|
+			balance += transaction[:amount]
+		end
+		return balance
+	end
+
+	def to_s
+		"Name: #{name}, Balance: #{sprintf("%0.2f", balance)}"
+	end
 end
 
 bank_account = BankAccount.new("Jason")
 bank_account.credit("Paycheck", 100)
 bank_account.debit("Groceries", 40)
-puts bank_account.inspect
+puts bank_account
+# puts sprintf("%0.2f", bank_account.balance)
 
 # puts bank_account.inspect
 # bank_account.add_transaction("Groceries", 40)
