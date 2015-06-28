6d19df261a56dd9c8eb128bd5c020dd701475732
leopoldkwok <leopoldkwok@gmail.com>
2015-06-28 19:45:52 +0100

    Instance Variables and Local Variables

--- 03_learn_ruby/06_ruby_objects_and_classes/name.rb
+++ 03_learn_ruby/06_ruby_objects_and_classes/name.rb
@@ -1,5 +1,6 @@
 class Name
 	attr_accessor :title, :first_name, :middle_name, :last_name
+	attr_reader :first_and_middle_name
 
 
 	def initialize(title, first_name, middle_name, last_name)
@@ -10,7 +11,10 @@ class Name
 	end
 
 	def full_name
-		@first_name + " " + @middle_name + " " + @last_name
+		@first_and_middle_name = @first_name + " " + @middle_name
+
+		# @first_name + " " + @middle_name + " " + @last_name
+		@first_and_middle_name + " " + @last_name
 	end
 
 	def full_name_with_title
