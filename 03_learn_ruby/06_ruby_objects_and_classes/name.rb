84c904e23089679c509795aa3183ca1632504f1e
leopoldkwok <leopoldkwok@gmail.com>
2015-06-28 20:03:02 +0100

    The to_s method

--- 03_learn_ruby/06_ruby_objects_and_classes/name.rb
+++ 03_learn_ruby/06_ruby_objects_and_classes/name.rb
@@ -1,6 +1,5 @@
 class Name
 	attr_accessor :title, :first_name, :middle_name, :last_name
-	attr_reader :first_and_middle_name
 
 
 	def initialize(title, first_name, middle_name, last_name)
@@ -11,24 +10,31 @@ class Name
 	end
 
 	def full_name
-		@first_and_middle_name = @first_name + " " + @middle_name
+		@first_name + " " + @middle_name + " " + @last_name
 
+		# @first_and_middle_name = @first_name + " " + @middle_name
 		# @first_name + " " + @middle_name + " " + @last_name
-		@first_and_middle_name + " " + @last_name
+		# @first_and_middle_name + " " + @last_name
 	end
 
 	def full_name_with_title
 		@title + " " + full_name()
+	end
+
+	def to_s
+		full_name_with_title
 	end
 end
 
 
 
 name = Name.new("Mr.", "Jason", "", "Seifer")
-puts name.full_name_with_title
+# puts name.full_name_with_title
+puts name
+puts name.inspect
 
-nick = Name.new("Mr.", "Nick", "", "Pettit")
-puts nick.full_name_with_title
+# nick = Name.new("Mr.", "Nick", "", "Pettit")
+# puts nick.full_name_with_title
 
 # puts "Title: #{name.title}"
 # name.title = "Dr."
