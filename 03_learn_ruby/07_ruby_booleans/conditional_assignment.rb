9790041e930c4c1ceced31447e951252b6fb0ea5
leopoldkwok <leopoldkwok@gmail.com>
2015-06-29 22:25:23 +0100

    conditional assignment

---
+++ 03_learn_ruby/07_ruby_booleans/conditional_assignment.rb
@@ -0,0 +1,18 @@
+# name = "Jason"
+
+# if defined?(name)
+# 	name
+# else
+# 	name = "Andrew"
+# end
+
+# puts name
+
+# new_name = "Jason"
+# new_name ||= "Andrew"
+# puts new_name
+# exit
+
+other_name = "Jason"
+other_name = (other_name || "Nick")
+puts other_name
