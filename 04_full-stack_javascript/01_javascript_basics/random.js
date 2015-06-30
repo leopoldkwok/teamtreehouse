54bba7fe25577c3c465baf80c4cb389b8a59c830
leopoldkwok <leopoldkwok@gmail.com>
2015-07-01 00:47:19 +0100

    Giving Information to Functions

--- 04_full-stack_javascript/01_javascript_basics/random.js
+++ 04_full-stack_javascript/01_javascript_basics/random.js
@@ -1,8 +1,22 @@
-function getRandomNumber() {
-	var randomNumber = Math.floor(Math.random() * 6) + 1;
+function getRandomNumber(upper) {
+	var randomNumber = Math.floor(Math.random() * upper) + 1;
 	return randomNumber;
 }
 
-alert(getRandomNumber());
-console.log(getRandomNumber());
-var dieRoll = getRandomNumber();
+
+
+// console.log(getRandomNumber(6));
+// console.log(getRandomNumber(100));
+// console.log(getRandomNumber(10000));
+// console.log(getRandomNumber(2));
+
+// alert(getRandomNumber());
+// console.log(getRandomNumber());
+// var dieRoll = getRandomNumber();
+
+function getArea(width, length, unit) {
+	var area = width * length;
+	return area + " " + unit;
+}
+
+console.log(getArea(10, 20, 'sq ft'));
