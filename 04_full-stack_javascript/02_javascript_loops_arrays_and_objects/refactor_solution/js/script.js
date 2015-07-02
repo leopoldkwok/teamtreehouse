f363bd440a4dcf7c227fca1e2ab5da8046bc5847
leopoldkwok <leopoldkwok@gmail.com>
2015-07-03 00:07:18 +0100

    The Refactor Challenge, Part 2

--- 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/refactor_solution/js/script.js
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/refactor_solution/js/script.js
@@ -1,18 +1,29 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
+function randomRGB() {
+	return Math.floor(Math.random() * 256);
+}
+
+function randomColor() {
+	var color = 'rgb(';
+	color += randomRGB() + ',';
+	color += randomRGB() + ',';
+	color += randomRGB() + ')';
+	return color;	
+}
+
+function print(message) {
+	document.write(message);
+}
+
 for ( var i = 1; i<=100; i+=1) {
-	red = Math.floor(Math.random() * 256);
-	green = Math.floor(Math.random() * 256);
-	blue = Math.floor(Math.random() * 256);
-	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	red = randomRGB();
+	green = randomRGB();
+	blue = randomRGB();
+	rgbColor = randomColor();
 	html +='<div style="background-color: ' + rgbColor + '"></div>';
 
 }
 
-
-
-document.write(html);
+print(html);
