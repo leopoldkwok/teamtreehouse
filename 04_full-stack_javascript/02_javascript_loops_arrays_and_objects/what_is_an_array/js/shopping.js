db88befa05af40eb906d8fdd7ef243872331ca72
leopoldkwok <leopoldkwok@gmail.com>
2015-07-03 01:03:04 +0100

    Accessing Items in an Array

--- 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/what_is_an_array/js/shopping.js
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/what_is_an_array/js/shopping.js
@@ -1,5 +1,11 @@
-var shoppinglist = [
+var shopping = [
 	'carrots', 
 	'milk', 
 	'eggs'
 ];
+
+console.log(shopping[0]);
+
+console.log(shopping[1]);
+console.log(shopping[2]);
+console.log(shopping[3]);
