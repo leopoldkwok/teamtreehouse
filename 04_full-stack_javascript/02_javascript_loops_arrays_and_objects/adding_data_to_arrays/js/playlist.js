486ad3a2b187bce54ab9cbb5b893aa99395fcd34
leopoldkwok <leopoldkwok@gmail.com>
2015-07-03 01:19:11 +0100

    Adding Data to Arrays

---
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/adding_data_to_arrays/js/playlist.js
@@ -0,0 +1,7 @@
+var playlist = [];
+playlist.push('I Did It My Way');
+playlist.push('Respect', 'Imagine');
+playlist.unshift('Born to Run');
+playlist.unshift('Louie Louie', 'Maybellene');
+
+printlist(playlist);
