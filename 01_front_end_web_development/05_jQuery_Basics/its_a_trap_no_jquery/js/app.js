f28e41b27ced1a91f01fbef18083941932ef7e14
leopoldkwok <leopoldkwok@gmail.com>
2015-07-14 22:18:36 +0100

    Ways to Include jQuery in a Project

---
+++ 01_front_end_web_development/05_jQuery_Basics/its_a_trap_no_jquery/js/app.js
@@ -0,0 +1,19 @@
+//Hide Warning
+//Show Warning Slowly
+
+$(".warning").hide().show("slow");
+
+
+
+
+// $(function() {
+// 	$(".warning").hide().show("slow");
+// });
+
+
+
+// $(document).ready(function() {
+// 	$(".warning").hide().show("slow");
+// });
+
+
