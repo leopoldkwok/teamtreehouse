aad867d51781701d05a4a3eeb3b62672c163b780
leopoldkwok <leopoldkwok@gmail.com>
2015-07-15 14:33:46 +0100

    Perfect

--- 01_front_end_web_development/05_jQuery_Basics/spoiler_revealer/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/spoiler_revealer/js/app.js
@@ -7,8 +7,9 @@ $(".spoiler span").hide();
 $(".spoiler").append("<button>Reveal Spoiler!</button>");
 //3. When button pressed
 $("button").click(function(){
-	//3.1 Show spoiler
-	$(".spoiler span").show();
+	//3.1 Show spoiler next to the button clicked
+	$(this).prev().show();
+	// $(".spoiler span").show();
 	// 3.2 Get rid of button
 	$(this).remove();
 });
