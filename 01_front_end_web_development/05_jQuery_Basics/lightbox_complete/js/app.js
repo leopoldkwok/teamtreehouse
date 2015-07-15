62d9a985a55ab87bf0230e801149be7b44f873d5
leopoldkwok <leopoldkwok@gmail.com>
2015-07-15 22:01:56 +0100

    Perform: Part 4

--- 01_front_end_web_development/05_jQuery_Basics/lightbox_complete/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/lightbox_complete/js/app.js
@@ -3,13 +3,18 @@
 
 var $overlay = $('<div id="overlay"</div>');
 var $image = $("<img>");
+var $caption = $("<p></p>");
 
 // An image to overlay
 $overlay.append($image);
 
+// A caption to overlay
+$overlay.append($caption);
+
+
 // Add overlay
 $("body").append($overlay);
-  // A caption
+  
 
 // Capture the click event on a link to an image
 $("#imageGallery a").click(function(event) {
@@ -22,12 +27,13 @@ $("#imageGallery a").click(function(event) {
   $overlay.show();
     
  // Get child's alt attribute and set caption
-
+ var captionText = $(this).children("img").attr("alt");
+ $caption.text(captionText);
 
 });
 
 // When overlay is clicked
-$overlay.click(function() {
+  $overlay.click(function() {
 // Hide the overlay.
   $overlay.hide();
 
