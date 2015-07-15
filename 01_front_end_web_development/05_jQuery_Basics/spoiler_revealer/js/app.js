fedd8af9e383e900a00519ac84b87725127f0b92
leopoldkwok <leopoldkwok@gmail.com>
2015-07-15 13:51:22 +0100

    Plan

--- 01_front_end_web_development/05_jQuery_Basics/spoiler_revealer/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/spoiler_revealer/js/app.js
@@ -1,6 +1,17 @@
 //Prevent spoilerphobes from seeing spoilers
 //Solution: Hide spoilers and reveal them through user interaction
 
+//1. Hide spoiler
+//2. Add a button
+//3. When button pressed
+	//3.1 Show spoiler
+	// 3.2 Get rid of button
+	
+
+
+
+
+
 // //1, Hide spoiler
 // $(".spoiler span").hide();
 // //2, Add a button
