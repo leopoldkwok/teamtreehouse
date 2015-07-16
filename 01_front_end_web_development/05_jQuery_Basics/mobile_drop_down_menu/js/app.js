8c265516fe79be83a29019939062bf5d8153919d
leopoldkwok <leopoldkwok@gmail.com>
2015-07-17 00:21:10 +0100

    Perform Part 3

--- 01_front_end_web_development/05_jQuery_Basics/mobile_drop_down_menu/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/mobile_drop_down_menu/js/app.js
@@ -1,40 +1,31 @@
 //Problem: It look gross in smaller browser widths and small devices
 //Solution: To hide the text links and swap them out with a more appropriate navigation
 
-//Create a select and append to menu
+//Create a select and append to #menu
 var $select = $("<select></select>");
 $("#menu").append($select);
-//Cycle over menu links
-$("#menu a").each(function() {
 
+//Cycle over menu links
+$("#menu a").each(function(){
   var $anchor = $(this);
-  // Create an option
+  //Create an option
   var $option = $("<option></option>");
 
-  //Options value is the href
+  //Deal with selected options depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+  //option's value is the href
   $option.val($anchor.attr("href"));
-
-  // options text is the text of link
-//   var anchorText = $anchor.text();
- $option.text(anchor.text());
-
-
-  // $option.text($anchor.text());
-  
-  // append option to select
+  //option's text is the text of link
+  $option.text($anchor.text());
+  //append option to select
   $select.append($option);
 });
 
-//Create button 
-var $button = $("<button>Go</button>");
-$("#menu").append($button);
-  //Bind click to button 
-$button.click(function() {
-  //go to selects location
+//Bind change listener to the select
+$select.change(function(){
+  //Go to select's location
   window.location = $select.val();
+});
 
-}); 
-
-// Modify CSS to hide links on small width and show button and select 
-  // Also hides select and button on larger width and shows links
-  // Deal with selected options depending on current page
