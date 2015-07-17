8b5e6fa1dc47007758bd573c2b7e4adc3ba11595
leopoldkwok <leopoldkwok@gmail.com>
2015-07-17 21:01:03 +0100

    Perform: Part 1

--- 01_front_end_web_development/05_jQuery_Basics/password_confirmation_form/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/password_confirmation_form/js/app.js
@@ -2,12 +2,20 @@
 // solution: hide and show them at appropriate times
 
 // Hide Hints
+$("form span").hide();
 
-  // when event happens on password input
-
-// Find out if password is valid
-  // Hide hint if valid
+function passwordEvent() {
+  // Find out if password is valid
+  if($(this).val().length > 8) {
+   // Hide hint if valid
+    $(this).next().hide();
+  } else {
   //else show hint
+    $(this).next().show();
+  }
+}
+  // when event happens on password input
+$("#password").focus(passwordEvent).keyup(passwordEvent);
 
 // When event happens on confirmation input
   // find out if password and confirmation match
