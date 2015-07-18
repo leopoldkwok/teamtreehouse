f563520ec11f44e8ebce4c16815242e7b5388a6d
leopoldkwok <leopoldkwok@gmail.com>
2015-07-18 10:18:56 +0100

    Plan

--- 01_front_end_web_development/05_jQuery_Basics/simple_drawing_application/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/simple_drawing_application/js/app.js
@@ -1,2 +1,21 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
+
+// When clicking on control list items
+  // Deselect sibling elements
+  // Select clicked element
+
+  //when add color is pressed
+    //show color Select or hide the colour select
+
+// When colour sliders change 
+  // update the new color span
+
+// When add color is pressed
+  //Append the colour to the controls ul
+  // Select the new colour
+
+// On mouse events on the canvas
+  //Draw lines
+
+  
