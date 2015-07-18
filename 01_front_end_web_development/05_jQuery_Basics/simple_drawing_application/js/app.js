4685d5040cfb83a9c92b44a6ad8c65f3601314f4
leopoldkwok <leopoldkwok@gmail.com>
2015-07-18 10:37:31 +0100

    Perform: PArt 1

--- 01_front_end_web_development/05_jQuery_Basics/simple_drawing_application/js/app.js
+++ 01_front_end_web_development/05_jQuery_Basics/simple_drawing_application/js/app.js
@@ -1,21 +1,28 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
 
+var color = $(".selected").css("background-color"); //use the color in the future
+
 // When clicking on control list items
+$(".controls li ").click(function() {
   // Deselect sibling elements
+  $(this).siblings().removeClass("selected");
   // Select clicked element
+  $(this).addClass("selected");
+  //cached current color
+  color = $(this).css("background-color");
+});
+  
 
   //when add color is pressed
     //show color Select or hide the colour select
 
 // When colour sliders change 
-  // update the new color span
+  // update the new colour span
 
-// When add color is pressed
+// When add colour is pressed
   //Append the colour to the controls ul
   // Select the new colour
 
 // On mouse events on the canvas
   //Draw lines
-
-  
