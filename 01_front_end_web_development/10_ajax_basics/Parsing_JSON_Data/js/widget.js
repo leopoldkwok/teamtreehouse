23f23a3270152af2bbe7f63ecb1ec5847faf365f
leopoldkwok <leopoldkwok@gmail.com>
2015-07-28 08:45:39 +0100

    Stage 2 Challenge

--- 01_front_end_web_development/10_ajax_basics/Parsing_JSON_Data/js/widget.js
+++ 01_front_end_web_development/10_ajax_basics/Parsing_JSON_Data/js/widget.js
@@ -18,3 +18,24 @@ xhr.onreadystatechange = function () {
   }
 };
 xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.onreadystatechange = function () {
+  if(roomRequest.readyState === 4 && roomRequest.status === 200) {
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomRequest.send();
