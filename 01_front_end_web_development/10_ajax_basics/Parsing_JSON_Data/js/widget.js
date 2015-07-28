b72f41f0bbf03f1e53b16fc111fe40dbf608aed7
leopoldkwok <leopoldkwok@gmail.com>
2015-07-28 01:18:47 +0100

    Processing JSON data

--- 01_front_end_web_development/10_ajax_basics/Parsing_JSON_Data/js/widget.js
+++ 01_front_end_web_development/10_ajax_basics/Parsing_JSON_Data/js/widget.js
@@ -1,9 +1,20 @@
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-	if(xhr.readystate === 4) {
-		var employees = JSON.parse(xhr.responseText);
-    console.log(employees);
-	}
+xhr.open('GET', '../data/employees.json');
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
 };
-xhr.open('GET', 'data/employees.json');
 xhr.send();
