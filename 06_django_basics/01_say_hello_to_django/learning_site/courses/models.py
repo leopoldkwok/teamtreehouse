200f13162836be017c67a867fcfcd143b96c6d95
leopoldkwok <leopoldkwok@gmail.com>
2015-07-28 23:21:14 +0100

    Adding Instances

--- 06_django_basics/01_say_hello_to_django/learning_site/courses/models.py
+++ 06_django_basics/01_say_hello_to_django/learning_site/courses/models.py
@@ -5,3 +5,6 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+    	return self.title
