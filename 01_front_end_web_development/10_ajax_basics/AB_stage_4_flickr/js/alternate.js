372cee9ca3cfb85643d68b380d30afad6022537f
leopoldkwok <leopoldkwok@gmail.com>
2015-07-30 14:15:44 +0100

    Making the AJAX Request

---
+++ 01_front_end_web_development/10_ajax_basics/AB_stage_4_flickr/js/alternate.js
@@ -0,0 +1,9 @@
+$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+	{
+		tags: animal,
+		format: "json"
+	},
+	function (data) {
+
+	}
+	);
--- 01_front_end_web_development/10_ajax_basics/AB_stage_4_flickr/js/flickr.js
+++ 01_front_end_web_development/10_ajax_basics/AB_stage_4_flickr/js/flickr.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
 		$('button').removeClass("selected");
 		$(this).addClass("selected");
 		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-		var animal = $(this).text(); 
+		var animal = $(this).text();
+		var flickrOptions = {
+			tags:animal,
+			format: "json"
+		};
+		function displayPhotos(data) {
+			
+		} 
 		$.getJSON(flickerAPI, flickrOptions, displayPhotos);
 	});
 }); // end ready
