5cbd063a2d9181fa2648d0f6a10ff21c62cf5d53
leopoldkwok <leopoldkwok@gmail.com>
2015-07-30 18:01:02 +0100

    Displaying the Photos

--- 01_front_end_web_development/10_ajax_basics/AB_stage_4_flickr/js/flickr.js
+++ 01_front_end_web_development/10_ajax_basics/AB_stage_4_flickr/js/flickr.js
@@ -9,7 +9,14 @@ $(document).ready(function() {
 			format: "json"
 		};
 		function displayPhotos(data) {
-			
+			var photoHTML = '<ul>';
+			$.each(data.items, function(i, photo) {
+				photoHTML += '<li class="grid-25 tablet-grid-50">';
+				photo += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"></a></li>';  
+			});
+			photoHTML += '</ul>';
+			$('#photos').html(photoHTML);
 		} 
 		$.getJSON(flickerAPI, flickrOptions, displayPhotos);
 	});
