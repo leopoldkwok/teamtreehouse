677a6fe84c7fab8c3ea44d5bb63cb4338c942a51
leopoldkwok <leopoldkwok@gmail.com>
2015-08-01 20:01:55 +0100

    Understanding This

--- 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
+++ 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
@@ -1,7 +1,15 @@
 var dice = {
+	sides: 6,
 	roll: function () {
-	  var sides = 6;
-	  var randomNumber = Math.floor(Math.random() * sides) + 1;
-	  console.log(randomNumber);
+		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+		console.log(randomNumber);
+	}
+}
+
+var dice10 = {
+	sides: 10,
+	roll: function () {
+		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+		console.log(randomNumber);
 	}
 }
