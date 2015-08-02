6c77b10f6c034ee2a655feca67aaf7ab3f63aa92
leopoldkwok <leopoldkwok@gmail.com>
2015-08-02 19:23:00 +0100

    Challenge Solution

--- 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
+++ 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
@@ -1,7 +1,22 @@
-var dice = {
-	sides: 6,
-	roll: function () {
+function Dice(sides) {
+	this.sides = sides;
+	this.roll = function () {
 		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
 		return(randomNumber);
 	}
 }
+
+var dice = new Dice(6);
+
+
+
+
+
+
+// var dice = {
+// 	sides: 6,
+// 	roll: function () {
+// 		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+// 		return(randomNumber);
+// 	}
+// }
