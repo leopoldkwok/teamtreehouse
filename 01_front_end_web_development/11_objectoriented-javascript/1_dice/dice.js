c874ee750a51144fbe6836cd9718f442e9afc985
leopoldkwok <leopoldkwok@gmail.com>
2015-08-02 19:40:01 +0100

    Methods with Prototypes

--- 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
+++ 01_front_end_web_development/11_objectoriented-javascript/1_dice/dice.js
@@ -1,12 +1,16 @@
 function Dice(sides) {
 	this.sides = sides;
-	this.roll = function () {
-		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-		return(randomNumber);
-	}
 }
 
 var dice = new Dice(6);
+var dice10 = new Dice(10);
+
+Dice.prototype.roll = function() {
+		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+		return(randomNumber);
+}
+
+console.log(dice.roll === dice10.roll);
 
 
 
