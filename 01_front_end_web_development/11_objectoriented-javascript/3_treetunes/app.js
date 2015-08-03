2ddf92ce338c5a91961e46624cbd97cc8264dc94
leopoldkwok <leopoldkwok@gmail.com>
2015-08-03 17:59:19 +0100

    Building the Movie Object

--- 01_front_end_web_development/11_objectoriented-javascript/3_treetunes/app.js
+++ 01_front_end_web_development/11_objectoriented-javascript/3_treetunes/app.js
@@ -3,9 +3,12 @@ var playlist = new Playlist();
 var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2.54");
 var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
 
+var manOfSteel = new Movie("Man of Steel", 2013, "2:23:00");
+
 playlist.add(hereComesTheSun);
 playlist.add(walkingOnSunshine);
 
+playlist.add(manOfSteel);
 var playlistElement = document.getElementById("playlist");
 
 playlist.renderInElement(playlistElement);
