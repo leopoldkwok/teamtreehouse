edbb35f9359757ea630332253f2efc4e3d10a09c
leopoldkwok <leopoldkwok@gmail.com>
2015-08-03 09:17:50 +0100

    Building the Media Object

---
+++ 01_front_end_web_development/11_objectoriented-javascript/3_treetunes/media.js
@@ -0,0 +1,14 @@
+function Media(title, duration) {
+	this.title = title;
+	this.duration = duration;
+	this.isPlaying = false;
+
+}
+
+Media.prototype.play = function() {
+	this.isPlaying = true;
+};
+
+Media.prototype.stop = function() {
+	this.isPlaying = false;
+};
