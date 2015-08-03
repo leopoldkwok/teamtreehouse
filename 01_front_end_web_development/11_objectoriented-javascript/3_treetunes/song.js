d117701f5699741939ae77a4fd5b57e5b2ef9eca
leopoldkwok <leopoldkwok@gmail.com>
2015-08-03 17:54:13 +0100

    Updating the Song Object

--- 01_front_end_web_development/11_objectoriented-javascript/3_treetunes/song.js
+++ 01_front_end_web_development/11_objectoriented-javascript/3_treetunes/song.js
@@ -1,18 +1,10 @@
 function Song(title, artist, duration) {
-	this.title = title;
+	// var song = this;
+	Media.call(this, title, duration);
 	this.artist = artist;
-	this.duration = duration;
-	this.isPlaying = false;
-
 }
 
-Song.prototype.play = function() {
-	this.isPlaying = true;
-};
-
-Song.prototype.stop = function() {
-	this.isPlaying = false;
-};
+Song.prototype = Object.create(Media.prototype);
 
 Song.prototype.toHTML = function() {
 	var htmlString = '<li';
