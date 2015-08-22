69040857b5ae524fd62258f7be625e148af2ffeb
leopoldkwok <leopoldkwok@gmail.com>
2015-08-22 21:23:15 +0100

    PHP Basic Usage

---
+++ 09_php_development/01_php_basics/project/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title>Mike The Frog | Treehouse Profile</title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+  
+  <body>
+    <section class="sidebar text-center">
+      <div class="avatar">
+        <img src="img/avatar.png" alt="Mike The Frog">
+      </div>
+      <h1>Mike Frog</h1>
+      <p>Portland, OR</p>
+      <hr />
+      <p>Welcome to PHP Basics!</p>
+      <hr />
+      <ul class="social">
+        <li><a href=""><span class="icon twitter"></span></a></li>
+      </ul>
+    </section>
+    <section class="main">
+      <p>Let's Get Started!</p>
+      <p><?php echo "Hello, World!" ?></p>
+    </section>
+  </body>
+</html>
