22a603f1af5994c63cca2b55ac57dfe8a809f35b
leopoldkwok <leopoldkwok@gmail.com>
2015-08-31 22:57:47 +0100

    Named Routes

--- 09_php_development/04_building_websites_with_php/index.php
+++ 09_php_development/04_building_websites_with_php/index.php
@@ -1,35 +1,33 @@
 <?php
 require 'vendor/autoload.php';
-date_default_timezone_set('Europe/London');
+date_default_timezone_set('America/New_York');
 
-//use Monolog\Logger;
-//use Monolog\Handler\StreamHandler;
-//
-//$log = new Logger('name');
-//$log->pushHandler(new StreamHandler('app.txt', Logger::WARNING));
+//$log = new Monolog\Logger('name');
+//$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
 //$log->addWarning('Oh Noes.');
-//
-//echo 'Hello World!';
 
-$app = new \Slim\Slim(array(
+$app = new \Slim\Slim( array(
   'view' => new \Slim\Views\Twig()
 ));
 
+$view = $app->view();
 $view->parserOptions = array(
     'debug' => true
 );
-
 $view->parserExtensions = array(
     new \Slim\Views\TwigExtension(),
 );
 
-
-$app->get('/',function() use($app){
+$app->get('/', function() use($app){
   $app->render('about.twig');
-});
+})->name('home');
 
-$app->get('/contact',function() use($app){
+$app->get('/contact', function() use($app){
   $app->render('contact.twig');
+})->name('contact');
+
+$app->post('/contact', function() use($app){
+  var_dump($app->request->post());
 });
 
 $app->run();
