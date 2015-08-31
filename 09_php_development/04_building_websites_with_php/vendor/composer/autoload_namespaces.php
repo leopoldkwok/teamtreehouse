6c44e0e2a8d417658f06485b9b7dde19496b842f
leopoldkwok <leopoldkwok@gmail.com>
2015-08-31 18:16:57 +0100

    Installing Twig

--- 09_php_development/04_building_websites_with_php/vendor/composer/autoload_namespaces.php
+++ 09_php_development/04_building_websites_with_php/vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Slim' => array($vendorDir . '/slim/slim'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
 );
