14b5275cbfc41e38218d673d0a0551232210a43d
leopoldkwok <leopoldkwok@gmail.com>
2015-09-13 21:57:08 +0100

    ordering the result set

--- 09_php_development/05_database_foundation/01_installing_mysqlserver_and_mysql_workbench/script.sql
+++ 09_php_development/05_database_foundation/01_installing_mysqlserver_and_mysql_workbench/script.sql
@@ -5,3 +5,12 @@ select * from actors;
 select  title FROM movies;
 
 select * from movies where title LIKE "%godfather%"; 
+
+select * from movies order by year;
+
+select * from movies order by year DESC;
+
+select * from movies order by year ASC;
+
+select * from movies order by year ASC, title DESC;
+
