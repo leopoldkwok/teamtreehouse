d418ccf317273b293c17cd55a7481beb1bffb0ce
leopoldkwok <leopoldkwok@gmail.com>
2016-06-08 23:54:21 +0100

    challenge

---
+++ 04_full-stack_javascript/01_javascript_basics/04_challenge/scripts.js
@@ -0,0 +1,4 @@
+alert("Help me fix this program!");
+alert("Can you get this message to appear?");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>I'm practicing 'debugging'.</p>");
