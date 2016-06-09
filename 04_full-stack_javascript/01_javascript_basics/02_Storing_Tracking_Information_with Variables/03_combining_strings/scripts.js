d51ceafe4a15b27f13d65e41bfca4cfe645c0ae4
leopoldkwok <leopoldkwok@gmail.com>
2016-06-09 08:25:16 +0100

    combining strings

---
+++ 04_full-stack_javascript/01_javascript_basics/02_Storing_Tracking_Information_with Variables/03_combining_strings/scripts.js
@@ -0,0 +1,7 @@
+var visitor = prompt('What is your name?');
+var message = 'Hello ' + visitor + ". Welcome to Treehouse."
+message += "We are so glad that you came by to visit,"
+message += visitor;
+message += ". Please come again, when you want to learn some more.";
+
+document.write(message);
