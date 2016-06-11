2d62cb56d3bc1306d02bea28dc5c539805dae2a7
leopoldkwok <leopoldkwok@gmail.com>
2016-06-11 16:10:03 +0100

    numbers and strings

---
+++ 04_full-stack_javascript/01_javascript_basics/03_working_with_numbers/02_numbers_and_strings/scripts.js
@@ -0,0 +1,4 @@
+var HTMLBadges = prompt('How many HTML badges do you have?');
+var CSSBadges = prompt('How many CSS badges do you have?');
+var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+alert('Wow, You have ' + totalBadges + ' badges!');
