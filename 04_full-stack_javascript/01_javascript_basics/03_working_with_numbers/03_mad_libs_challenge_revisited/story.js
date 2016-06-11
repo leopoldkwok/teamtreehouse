f3b0baea021a6c74f0e0ab10f9e64ed9425e3e74
leopoldkwok <leopoldkwok@gmail.com>
2016-06-11 23:15:18 +0100

    The Mad Libs Challenge Revisited

---
+++ 04_full-stack_javascript/01_javascript_basics/03_working_with_numbers/03_mad_libs_challenge_revisited/story.js
@@ -0,0 +1,14 @@
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questons left]';
+var adjective = prompt('Please type an adjective' + questionsLeft);
+questions -=1;
+questionsLeft = ' [' + questions + ' questions left]';
+var verb = prompt('Please type a verb' + questionsLeft);
+questions -=1;
+questionsLeft = ' [' + questions + ' questions left]';
+var noun = prompt('Please type a noun' + questionsLeft);
+alert('All done. Ready for the message?');
+var sentence = "<h2>There once was a " + adjective;
+sentence += ' programmer who wanted to use JavaScript to ' + verb;
+sentence += ' the ' + noun + '.</h2>';
+document.write(sentence);
