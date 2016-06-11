021db006d21350e5b1c2181c5e27c45727bd9ac0
leopoldkwok <leopoldkwok@gmail.com>
2016-06-11 15:07:38 +0100

    doing math

---
+++ 04_full-stack_javascript/01_javascript_basics/03_working_with_numbers/01_doing_math/scripts.js
@@ -0,0 +1,7 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write(secondsPerDay);
