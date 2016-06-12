b93b0053bf724987b403a7ab03e2428e27cd6a77
leopoldkwok <leopoldkwok@gmail.com>
2016-06-12 08:39:21 +0100

    introducing conditional statements

---
+++ 04_full-stack_javascript/01_javascript_basics/04_Making Decisions with Conditional Statements/01_introducing_conditional_statements/quiz.js
@@ -0,0 +1,6 @@
+var answer = prompt('What programming language is the name of a gem?');
+if (answer.toUpperCase() === 'RUBY') {
+  document.write("<p>That's right!</p>");
+} else {
+  document.write("<p>Sorry, that's wrong.</p>");
+}
