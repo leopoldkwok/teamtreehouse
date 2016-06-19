980b077f7a2ed0229723d7fdfcb9f36c34715208
leopoldkwok <leopoldkwok@gmail.com>
2016-06-19 15:21:35 +0100

    reorganising folders

---
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/01_Simplify Repetitive Tasks with Loops/03_do_while_loops/scripts.js
@@ -0,0 +1,21 @@
+var randomNumber = getRandomNumber(10);
+var guess; // holds the player guess
+var guessCount = 0;
+var correctGuess = false;
+
+function getRandomNumber(upper) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
+}
+
+do {
+  guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+  guessCount += 1; // increment the guess counter
+  if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+  }
+} while (! correctGuess)
+
+
+document.write('<h1>You guessed the number!</h1>');
+document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
