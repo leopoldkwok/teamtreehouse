51dc51580dfcdc2c58c1f8bb99acbf5b97dc8e5e
leopoldkwok <leopoldkwok@gmail.com>
2016-06-21 08:36:30 +0100

    build a quiz challenge part 2 solution

---
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/02_Tracking_Multiple Items_with_Arrays/08_build_a_quiz_challenge_part2_solution/js/quiz.js
@@ -0,0 +1,47 @@
+var questions = [
+  ['How many states are in the United States?', 50],
+  ['How many continents are there?', 7],
+  ['How many legs does an insect have?', 6]
+];
+
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var correct = [];
+var wrong = [];
+var html;
+
+for (var i = 0; i < questions.length; i +=1) {
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = parseInt(prompt(question));
+  if (response === answer) {
+    correctAnswers +=1;
+    correct.push(question);
+  } else {
+    wrong.push(question);
+  }
+}
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+ // create an ordered list
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for(var i = 0; i < arr.length; i +=1) {
+      listHTML +='<li>' + arr[i] + '</li>';
+    }
+    listHTML +='</ol>';
+    return listHTML;
+}
+
+html = "You got " + correctAnswers + " question(s) right.";
+html +='<h2>You got these questions correct:</h2>';
+html +=buildList(correct);
+html +='<h2>You got these questions wrong: </h2>';
+html += buildList(wrong);
+print(html);
