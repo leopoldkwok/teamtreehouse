fc258902d0bb8f267fb565d72afd6317c837e4d1
leopoldkwok <leopoldkwok@gmail.com>
2016-06-26 09:31:05 +0100

    05_The_Build_an_Object_ Challenge_Part_2_Solution

---
+++ 04_full-stack_javascript/02_javascript_loops_arrays_and_objects/03_Tracking Data Using Objects/05_The_Build_an_Object_ Challenge_Part_2_Solution/js/student_report.js
@@ -0,0 +1,17 @@
+var message = '';
+var student; /* hold a student object */
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+for (var i=0; i < students.length; i += 1) {
+  student = students[i];
+  message += '<h2>Student: ' + student.name + '</h2>';
+  message += '<p>Track: ' + student.track + '</p>';
+  message += '<p>Points: ' + student.points + '</p>';
+  message += '<p>Achievements: ' + student.achievements + '</p>';
+
+}
+print(message);
