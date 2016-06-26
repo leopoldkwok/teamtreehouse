0043c841604d1e78dad6ad63c6875fe26a12118f
leopoldkwok <leopoldkwok@gmail.com>
2016-06-26 19:31:59 +0100

    perfect

---
+++ 04_full-stack_javascript/03_jquery_basics/02_creating_a_Spoiler_Revealer/06_Perfect/js/app.js
@@ -0,0 +1,16 @@
+// prevent spoiler phones from seeing spoilers
+// Solution: hide spoilers and reveal them through user interaction
+
+// 1. Hide Spoiler
+$(".spoiler span").hide();
+// 2. Add a button
+$(".spoiler").append("<button>Reveal Spoiler!</button>");
+// 3. When button pressed
+$("button").click(function() {
+  // 3.1 Show the spoiler next to the button clicked
+  $(this).prev().show();
+  $(".spoiler span").show();
+  // 3.2 Get rid of button
+  $(this).remove();
+});
+
