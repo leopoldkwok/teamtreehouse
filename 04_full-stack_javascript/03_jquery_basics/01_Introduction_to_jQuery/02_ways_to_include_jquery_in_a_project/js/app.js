3400c4de20c6502522cf7d736bfed2d25f2fa353
leopoldkwok <leopoldkwok@gmail.com>
2016-06-26 16:00:45 +0100

    ways to include jquery in a project

---
+++ 04_full-stack_javascript/03_jquery_basics/01_Introduction_to_jQuery/02_ways_to_include_jquery_in_a_project/js/app.js
@@ -0,0 +1,29 @@
+// for when the dom is already loaded due to the javascript references at the bottom of the dom
+
+$(".warning").hide().show("slow");
+
+
+// anonmoyous function
+
+// $(function() {
+//   $(".warning").hide().show("slow");
+// });
+
+
+// Nameless function
+
+// $(document).ready(function() {
+//   $(".warning").hide().show("slow");
+// });
+
+// Named function
+
+// var myCode = function() {
+//   $(".warning").hide().show("slow");
+// }
+
+// function myCode() {
+//   $(".warning").hide().show("slow");
+// }
+
+// $(document).ready(myCode);
