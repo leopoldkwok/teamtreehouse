edbbabc165ea2f51e3d26e330ec22d6a254a3150
leopoldkwok <leopoldkwok@gmail.com>
2016-06-26 20:17:29 +0100

    plan

---
+++ 04_full-stack_javascript/03_jquery_basics/03_creating_a_simple lightbox/02_plan/js/app.js
@@ -0,0 +1,12 @@
+// problem: user when clicking on image goes to a dead end
+// solution: create an overlay with the large image - Light box
+
+// 1. Capture the click event on a link to an image
+  // 1.1  Show the overlay.
+  // 1.2 update overlay with the image linked in the link
+  // 1.3 Get child's alt altribute and set caption
+// 2. Add overlay
+  // 2.1 An image
+  // 2.2 A caption
+// 3. When overlay is clicked
+  // 3.1 Hide the overlay
