200f728f55ad185ed356d59d47b6f1d4e50a112e
leopoldkwok <leopoldkwok@gmail.com>
2016-06-26 17:05:15 +0100

    plan

---
+++ 04_full-stack_javascript/03_jquery_basics/02_creating_a_Spoiler_Revealer/02_plan/js/app.js
@@ -0,0 +1,8 @@
+// prevent spoiler phones from seeing spoilers
+// Solution: hide spoilers and reveal them through user interaction
+
+// 1. Hide Spoiler
+// 2. Add a button
+// 3. When button pressed
+  // 3.1 Show the spoiler
+  // 3.2 Get rid of button
