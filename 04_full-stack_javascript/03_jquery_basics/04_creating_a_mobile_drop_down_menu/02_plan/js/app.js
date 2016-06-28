a2a790460d04a202ac904ef977942d4d6ef17080
leopoldkwok <leopoldkwok@gmail.com>
2016-06-28 08:07:29 +0100

    update plan

--- 04_full-stack_javascript/03_jquery_basics/04_creating_a_mobile_drop_down_menu/02_plan/js/app.js
+++ 04_full-stack_javascript/03_jquery_basics/04_creating_a_mobile_drop_down_menu/02_plan/js/app.js
@@ -1,2 +1,15 @@
 // Problem: It looks gross in smaller browser widths and small devices
 // Solution: to hide the text links and swap them out with a more appropriate navigation
+
+// Create a select and append to menu
+// Cycle over menu links
+  // Create an option
+  // option's value is the href
+  // option's text is the text of link
+  // append option to select
+// Create button
+// Bind click to button
+  // Go to select's location
+// Modify CSS to hide links on small width and show button and select
+  // Also hides select and button on larger width and show's link
+// Deal with selected options depending on current page
