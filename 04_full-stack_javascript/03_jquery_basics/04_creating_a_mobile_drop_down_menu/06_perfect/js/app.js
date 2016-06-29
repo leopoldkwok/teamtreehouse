7a4ebf56f3897fe4fb6f436e17b21e8e25eeb5b4
leopoldkwok <leopoldkwok@gmail.com>
2016-06-29 08:01:22 +0100

    perfecting

---
+++ 04_full-stack_javascript/03_jquery_basics/04_creating_a_mobile_drop_down_menu/06_perfect/js/app.js
@@ -0,0 +1,36 @@
+// Problem: It looks gross in smaller browser widths and small devices
+// Solution: to hide the text links and swap them out with a more appropriate navigation
+
+// Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+// Cycle over menu links
+$("#menu a").each(function() {
+  var $anchor = $(this);
+  // Create an option
+  var option = $("<option></option");
+
+  // Deal with selected options depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+
+
+
+  // option's value is the href
+  $option.val($anchor.attr("href"));
+  // option's text is the text of link
+  $option.text($anchor.text());
+  // append option to select
+  $select.append($option);
+});
+
+// // Create button
+// var $button = $("<button>Go</button>");
+// $("#menu").append($button);
+
+// Bind change listener to the select
+$select.change(function() {
+  // Go to select's location
+  window.location = $select.val(); // getting the selected value
+});
