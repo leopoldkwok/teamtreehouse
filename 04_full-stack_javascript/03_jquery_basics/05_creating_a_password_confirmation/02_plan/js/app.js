9ca74eb281e8424be7df5ae647acd5a13c3e4af0
leopoldkwok <leopoldkwok@gmail.com>
2016-06-29 08:10:56 +0100

    plan

---
+++ 04_full-stack_javascript/03_jquery_basics/05_creating_a_password_confirmation/02_plan/js/app.js
@@ -0,0 +1,15 @@
+// Problem: Hints are shown when form is valid
+// Solution: Hide and show them at appropriate times
+
+// Hide hints
+// When event happens on password input
+  // Find out if password is valid
+    // Hide hint if valid
+    // else show hint
+
+// When event happens on confirmation input
+  // Fnd out if password and confirmation match
+  // Hide hint if matched
+  // else show hint
+
+
