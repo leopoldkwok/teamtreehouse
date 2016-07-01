d11fe6ccc19c47bc92db36e1a6db9374bfc1b27a
leopoldkwok <leopoldkwok@gmail.com>
2016-07-01 15:53:50 +0100

    Using Third Party Gulp Libraries

---
+++ 04_full-stack_javascript/16_gulp_basics/02_gulp-your-javascript-workflow/01_Using_Third_Party_Gulp_Libraries/Stage2Video1/gulpfile.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat');
+
+gulp.task("concatScripts", function() {
+    gulp.src([
+      'js/jquery.js',
+      'js/sticky/jquery.sticky.js',
+      'js/main.js'])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
+
+});
+
+gulp.task("default", ["hello"], function() {
+    console.log("the default task!!!!");
+});
