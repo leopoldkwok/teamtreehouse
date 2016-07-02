0fff8fdde486798c8caf657116e52f2dd06dac3e
leopoldkwok <leopoldkwok@gmail.com>
2016-07-03 00:17:58 +0100

    variables

---
+++ 15_ios_development_with_swift/01_swift_basics/01_variables/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+var str = "Hello, playground"
+
+"Hello, playground"
+
+str = "Hello, world!"
+
+str = 30
+
+var number = 10
+
+number = 20
+
+number = "30"
