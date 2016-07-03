67fcdabe8690b8f451a9674d2cba661b8a67c183
leopoldkwok <leopoldkwok@gmail.com>
2016-07-03 09:28:36 +0100

    strings

---
+++ 15_ios_development_with_swift_2.0/01_swift_2.0_basics/02_Swift_Types/01_swift_types/01_strings/SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+
+let address = country + ", " + state + ", " + city
+
+//: [Next](@next)
+
