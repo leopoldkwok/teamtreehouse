400ce1a7d6a9b3056d1045953a3767aee48fb1ed
leopoldkwok <leopoldkwok@gmail.com>
2016-07-03 17:41:49 +0100

    numbers and booleans

---
+++ 15_ios_development_with_swift_2.0/01_swift_2.0_basics/02_Swift_Types/03_numbers_and_booleans/SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+//////////////////////////////
+//////// STRINGS ////////////
+//////////////////////////////
+
+// Concatenation
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let street = "West Street"
+
+let concatendatedAddress = country + ", " + state + ", " + city
+
+// String Interpolation
+
+let interpolatedAddress = "\(country), \(state), \(city)"
+
+let interpolatedStreetAddress = "\(222) \(street)" + street
+
+//////////////////////////////
+//////// INTEGERS ////////////
+//////////////////////////////
+
+let favouriteProgrammingLanguage = "Swift"
+let year = 2014 // Int
+
+///////////////////////////////////////////
+//////// Floating Point Numbers //////////
+//////////////////////////////////////////
+
+var version = 2.0 // Double
+
+///////////////////////////////////
+//////// Boolean //////////////////
+///////////////////////////////////
+
+let isAwesome = true // Bool
+
