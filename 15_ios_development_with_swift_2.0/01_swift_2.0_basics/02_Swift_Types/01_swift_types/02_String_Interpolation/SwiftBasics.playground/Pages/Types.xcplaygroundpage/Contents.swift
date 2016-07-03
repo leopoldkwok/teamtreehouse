da0f2bd749c6d39fb775ed1db0ca6ddcfa676a5a
leopoldkwok <leopoldkwok@gmail.com>
2016-07-03 09:42:30 +0100

    string interpolation

---
+++ 15_ios_development_with_swift_2.0/01_swift_2.0_basics/02_Swift_Types/01_swift_types/02_String_Interpolation/SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+// Concatenation
+
+let country = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let street = "West Street"
+
+let concatendatedAddress = country + ", " + state + ", " + city
+
+// String Interpolation
+
+let interpolatedAddress = "\(country), \(state), \(city)"
+
+let interpolatedStreetAddress = "\(222) \(street)" + street
+
+//: [Next](@next)
+
