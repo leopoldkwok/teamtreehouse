35912cd9276e745113218a47737bac993fba18fb
leopoldkwok <leopoldkwok@gmail.com>
2016-07-03 21:47:04 +0100

    Minifying Javascript Files

---
+++ 04_full-stack_javascript/16_gulp_basics/02_gulp-your-javascript-workflow/02_Minifying_JavaScript_ Files/Stage2Video2/gulpfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename');
+
+gulp.task("concatScripts", function() {
+    gulp.src([
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'
+        ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("default", ["hello"], function() {
+    console.log("the default task!!!!");
+});
