bfd621f215c66763b0188cf7695ca96abddbaeae
leopoldkwok <leopoldkwok@gmail.com>
2016-07-04 08:38:46 +0100

    turn sass into css automatically

---
+++ 04_full-stack_javascript/16_gulp_basics/03_Compile_Sass_with_Gulp/01_turn_sass_into_css_automatically/Stage3Video1/gulpfile.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass');
+
+gulp.task("concatScripts", function() {
+    gulp.src([
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'
+        ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", function() {
+    gulp.src('js/app.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('js'))
+});
+
+gulp.task('compileSass', function() {
+    gulp.src("scss/application.scss")
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task("default", ["hello"], function() {
+    console.log("the default task!!!!");
+});
