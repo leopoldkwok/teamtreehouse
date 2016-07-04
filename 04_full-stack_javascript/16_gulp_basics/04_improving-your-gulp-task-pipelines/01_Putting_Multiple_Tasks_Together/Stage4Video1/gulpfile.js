f7678798dcab858be22f3b0e7550189521711242
leopoldkwok <leopoldkwok@gmail.com>
2016-07-04 14:49:37 +0100

    Putting Multiple Tasks Together

---
+++ 04_full-stack_javascript/16_gulp_basics/04_improving-your-gulp-task-pipelines/01_Putting_Multiple_Tasks_Together/Stage4Video1/gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([ // the return lets us know when the task is finished.
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+	return gulp.src("js/app.js")
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("scss/application.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('css'));
+})
+
+gulp.task("build", ['minifyScripts', 'compileSass']); // since concat is a dependency of minify scripts - this does not need to be in the build task
+
+gulp.task("default", ["build"]); // place the build task so all you need to call is gulp
+
