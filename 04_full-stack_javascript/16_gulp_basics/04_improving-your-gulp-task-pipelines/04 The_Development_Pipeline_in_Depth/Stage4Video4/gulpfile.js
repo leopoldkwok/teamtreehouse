e1155dec2ec8ebe8811aa90641af345cf517fc96
leopoldkwok <leopoldkwok@gmail.com>
2016-07-04 21:45:34 +0100

    The Development Pipeline in Depth

---
+++ 04_full-stack_javascript/16_gulp_basics/04_improving-your-gulp-task-pipelines/04 The_Development_Pipeline_in_Depth/Stage4Video4/gulpfile.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("scss/application.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('css'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']); // watches for the sass files and only runs compileSass task when they change
+  gulp.watch('js/main.js', ['concatScripts']);   // watching the main.js file, and then only runs the concat Scripts methods
+})
+
+gulp.task('clean', function() {
+  del(['dist', 'css/application.css*', 'js/app*.js*']);
+});
+
+gulp.task("build", ['minifyScripts', 'compileSass'], function() {
+  return gulp.src(["css/application.css", "js/app.min.js", 'index.html',
+                   "img/**", "fonts/**"], { base: './'})
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task("default", ["build"]);
