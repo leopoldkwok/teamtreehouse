2b8efe02ac3a0542a25c0462ab2a01ed8c092539
leopoldkwok <leopoldkwok@gmail.com>
2016-07-05 17:21:02 +0100

    peform part 4

---
+++ 04_full-stack_javascript/03_jquery_basics/06_creating_a_simple_drawing_application/06_perform_part_4/js/app.js
@@ -0,0 +1,61 @@
+// Problem: No user interaction causes no change to application
+// solution: when user interacts cause changes appropriately
+var color = $(".selected").css("background-color");
+var content = $("canvas")[0].getContext("2d");
+// document.getElementsByTagName("canvas")[0]
+    // same as above
+
+// when clcking on control list items
+$(".controls").on("click", "li", function() {
+  // deselect sibling elements
+  $(this).siblings().removeClass("selected");
+  // Select clicked element
+  $(this).addClass("selected");
+  // cache current color
+  color = $(this).css("background-color");
+});
+
+
+// When "new color" is pressed
+$("#revealColorSelect").click(function() {
+  // show color is select or hide the color select
+  changeColor();
+  $("#colorSelect").toggle();
+});
+
+// update the new color span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+}
+  // when color sliders change
+  $("input[type=range]").change(changeColor);
+
+  // when "add color" is pressed
+  $("#addNewColor").click(function() {
+    // append the color to the controls ul
+    var $newColor = $("<li></li>");
+    $newColor.css("background-color", $("#newColor").css("background-color"));
+    $(".controls ul").appned($newColor);
+    // Select the new color
+    $newColor.click();
+  });
+
+
+  // on mouse events on the canvas
+    // Draw lines
+
+    context.beginPath(); // start a path
+    context.moveTo(10,10);
+    context.lineTo(20,10);
+    context.lineTo(20,20);
+    context.lineTo(10,20);
+    context.closePath();
+    context.stroke();
+
+    context.stroke();
+
+
+
