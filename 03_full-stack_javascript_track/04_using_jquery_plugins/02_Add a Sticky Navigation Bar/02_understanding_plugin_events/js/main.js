530e306951ffa26eba759b00d67df0118d344d3d
leopoldkwok <leopoldkwok@gmail.com>
2016-08-07 19:27:19 +0100

    Understanding Plugin Events

---
+++ 03_full-stack_javascript_track/04_using_jquery_plugins/02_Add a Sticky Navigation Bar/02_understanding_plugin_events/js/main.js
@@ -0,0 +1,14 @@
+$(".animsition").animsition({
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg',
+  linkElement: 'header a',
+  inDuration: 1000,
+  outDuration: 500
+});
+
+$('.header').sticky();
+
+$('.header').on('sticky-start', function() {
+  $('.description').html('We build <strong>great</strong> apps');
+});
+
