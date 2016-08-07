fd13303e2d0fb4f88b676aead6de0133a904c7d1
leopoldkwok <leopoldkwok@gmail.com>
2016-08-07 19:03:22 +0100

    Adding Sticky Navigation

---
+++ 03_full-stack_javascript_track/04_using_jquery_plugins/02_Add a Sticky Navigation Bar/01_adding_sticky_navigation/js/main.js
@@ -0,0 +1,10 @@
+$(".animsition").animsition({
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg',
+  linkElement: 'header a',
+  inDuration: 1000,
+  outDuration: 500
+});
+
+$('.header').sticky();
+
