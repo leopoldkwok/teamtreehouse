21cd392abafed060883506a25d196ca5602a1d44
leopoldkwok <leopoldkwok@gmail.com>
2016-09-11 08:51:49 +0100

    processing json data

---
+++ 03_full-stack_javascript_track/06_ajax_basics/02_Programming_AJAX/03_Processing_JSON_Data/js/widget.js
@@ -0,0 +1,20 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    var employees = JSON.parse(xhr.responseText);
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML +='</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+   }
+};
+xhr.open('GET', 'data/employees.json');
+xhr.send();
