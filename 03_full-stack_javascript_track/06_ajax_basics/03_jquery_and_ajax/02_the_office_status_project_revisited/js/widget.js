bf4bc9e4ffc018678452faa6f8491f8d246ae9d4
leopoldkwok <leopoldkwok@gmail.com>
2016-09-14 10:21:47 +0100

    the office status project revisited

---
+++ 03_full-stack_javascript_track/06_ajax_basics/03_jquery_and_ajax/02_the_office_status_project_revisited/js/widget.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  var url = "../data/employees.json";
+    $.getJSON(url, function (response) {
+      var statusHTML = '<ul class="bulleted">';
+      $.each(response, function(index, employee) {
+        if (employee.inoffice === true) {
+          statusHTML +='<li class="in">';
+        } else {
+          statusHTML +='<li class="out">';
+        }
+        statusHTML += employee.name + '</li>';
+      });
+      statusHTML +='</ul>';
+      $('#employeeList').html(statusHTML);
+    }); // end getJSON
+}); // end ready
