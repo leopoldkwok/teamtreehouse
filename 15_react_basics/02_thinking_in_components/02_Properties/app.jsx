9529165969e6dffd72254f392acf04e07a987cfb
leopoldkwok <leopoldkwok@gmail.com>
2016-11-07 23:25:32 +0000

    properties

---
+++ 15_react_basics/02_thinking_in_components/02_Properties/app.jsx
@@ -0,0 +1,40 @@
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>{props.title}</h1>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Leopold Kwok
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">31</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="player">
+          <div className="player-name">
+            Andrew Chalkey
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">33</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
