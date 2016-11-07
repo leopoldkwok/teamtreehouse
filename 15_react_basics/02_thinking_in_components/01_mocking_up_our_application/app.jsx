006466bfed85cd7888d89db72d860252cbcb419e
leopoldkwok <leopoldkwok@gmail.com>
2016-11-07 23:18:27 +0000

    added another person to the scoreboard

--- 15_react_basics/02_thinking_in_components/01_mocking_up_our_application/app.jsx
+++ 15_react_basics/02_thinking_in_components/01_mocking_up_our_application/app.jsx
@@ -1,6 +1,6 @@
 function Application() {
   return (
-    <div className="application">
+    <div className="scoreboard">
       <div className="header">
         <h1>Scoreboard</h1>
       </div>
@@ -13,10 +13,25 @@ function Application() {
           <div className="player-score">
             <div className="counter">
               <button className="counter-action decrement"> - </button>
+              <div className="counter-score">31</div>
               <button className="counter-action increment"> + </button>
             </div>
           </div>
         </div>
+
+        <div className="player">
+          <div className="player-name">
+            Andrew Chalkey
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">33</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
