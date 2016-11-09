2907c8289b70f482efb0669bb5a84cdd9c5d8b51
leopoldkwok <leopoldkwok@gmail.com>
2016-11-09 01:27:56 +0000

    Communicating Events

---
+++ 15_react_basics/04_Designing_Data_Flow/02_Communicating_Events/app.jsx
@@ -0,0 +1,115 @@
+var PLAYERS = [
+  {
+    name: "Jim Hoskins",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Andrew Chalkley",
+    score: 35,
+    id: 2,
+  },
+  {
+    name: "Alena Holligan",
+    score: 42,
+    id: 3,
+  },
+]
+
+function Header(props) {
+  return(
+    <div className="header">
+        <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
+      <div className="counter-score">{props.score} </div>
+      <button className="counter-action increment"onClick={function() {props.onChange(+1);}}> + </button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+
+}
+
+function Player(props) {
+  return (
+    <div className="player">
+      <div className="player-name">
+        {props.name}
+      </div>
+      <div className="player-score">
+        <Counter score={props.score} onChange={props.onScoreChange} />
+      </div>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+  onScoreChange: React.PropTypes.func.isRequired,
+};
+
+var Application = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    initialPlayers: React.PropTypes.arrayOf(React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      score: React.PropTypes.number.isRequired,
+      id: React.PropTypes.number.isRequired,
+    })).isRequired,
+  },
+
+  getDefaultProps: function() {
+    return {
+      title: "Scoreboard",
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      players: this.props.initialPlayers,
+    };
+  },
+
+  onScoreChange: function(index, delta) {
+    console.log('onScoreChange', index, delta);
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  },
+
+  render: function() {
+      return (
+      <div className="scoreboard">
+        <Header title={this.props.title}/>
+
+        <div className="players">
+          {this.state.players.map(function(player, index) {
+            return (
+              <Player
+                onScoreChange={function(delta) {this.onScoreChange(index,delta)}.bind(this)}
+                name={player.name}
+                score={player.score}
+                key={player.id}/>
+            );
+          }.bind(this))}
+        </div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Application initialPlayers={PLAYERS}/>, document.getElementById('container'));
