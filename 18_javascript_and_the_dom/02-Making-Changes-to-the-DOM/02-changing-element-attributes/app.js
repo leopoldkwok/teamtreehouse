1a7557b316ca10c246856f7b9c1d20ec980c5cc9
leopoldkwok <leopoldkwok@gmail.com>
2017-01-17 08:18:45 +0000

    changing element attributes

---
+++ 18_javascript_and_the_dom/02-Making-Changes-to-the-DOM/02-changing-element-attributes/app.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input');
+
+const p = document.querySelector('p.description');
+
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', () => {
+  p.innerHTML = input.value + ':';
+});
+
+p.title = "List description";
